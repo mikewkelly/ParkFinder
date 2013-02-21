af0aff80d4588625bf9378bef8de607b1e12ec7c
Mike Kelly <mikewkelly@hotmail.com>
2013-02-21 09:37:28 -0800

    Partially completed parsers

--- ParkFinder/src/com/cpsc310/team_name/parkfinder/server/ParksListingParser.java
+++ ParkFinder/src/com/cpsc310/team_name/parkfinder/server/ParksListingParser.java
@@ -1,17 +1,100 @@
 package com.cpsc310.team_name.parkfinder.server;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
 
-import org.xml.sax.helpers.DefaultHandler;
+import com.cpsc310.team_name.parkfinder.client.Park;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.xml.client.DOMException;
+import com.google.gwt.xml.client.Document;
+import com.google.gwt.xml.client.Element;
+import com.google.gwt.xml.client.Node;
+import com.google.gwt.xml.client.XMLParser;
 
-public class ParksListingParser extends DefaultHandler{
+public class ParksListingParser {
 	
-	private URL theURL;
+	// URL of data: ftp://webftp.vancouver.ca/opendata/xml/parks_facilities.xml
 	
-	public ParksListingParser(URL aURL) {
-		theURL = aURL;
+	ArrayList parks = new ArrayList<Park>();
+	
+	public ParksListingParser() {
+		
 	}
 	
-	// TODO: Implement this class
+	public void parse() {
+		// TODO: implement this method using helper functions below
+	}
+	
+	
+	/** 
+	 * Called to return the Parks Listing data from the CoV server from URL:
+	 * ftp://webftp.vancouver.ca/opendata/xml/parks_facilities.xml
+	 * 
+	 * @return a String representing the XML file from the City of Vancouver Data Catalogue
+	 */
+	
+	private String downloadXMLString() {
+		String file = new String();
+		StringBuffer tempFile = new StringBuffer();
+		
+		tempFile.setLength(0);  // initialise the StringBuffer
+
+		try {
+			URL url = new URL("ftp://webftp.vancouver.ca/opendata/xml/parks_facilities.xml");
+			BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
+			String individualLine;
+
+			while ((individualLine = reader.readLine()) != null) {  // while non-empty lines exist:
+			 tempFile.append(individualLine);  // read each line and add it to tempFile
+			}
+			
+			reader.close();
+
+		} catch (MalformedURLException e) {
+			// Do nothing
+		} catch (IOException e) {
+			// Do nothing
+		}
+		
+		file = tempFile.toString();
+		return file;
+	}
+	
+	/**
+	 * Called to parse an XML String and create individual Park instances with
+	 * the data
+	 * 
+	 * @param file
+	 *            The XML file to be parsed
+	 */
+	private void parseXML(String file) {
+		try {
+			
+			// Convert XML String to DOM
+			Document fileDom = XMLParser.parse(file);
+			
+			// Collect required data and store as strings
+			
+			// parkId
+			Node idNode = fileDom.getElementsByTagName("Park").item(0);
+			String parkId = ((Element)idNode).getAttribute("ID");
+			
+			// TODO: complete this method
+		
+		
+		
+		
+		
+		} catch (DOMException e) {
+			Window.alert("Could not parse XML document");
+		}
+		
+		
+	}
+	
 
 }
--- ParkFinder/src/com/cpsc310/team_name/parkfinder/server/WeekendPlayfieldStatusParser.java
+++ ParkFinder/src/com/cpsc310/team_name/parkfinder/server/WeekendPlayfieldStatusParser.java
@@ -1,17 +1,77 @@
 package com.cpsc310.team_name.parkfinder.server;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
 
-import org.xml.sax.helpers.DefaultHandler;
+public class WeekendPlayfieldStatusParser {
 
-public class WeekendPlayfieldStatusParser extends DefaultHandler {
+	// URL: ftp://webftp.vancouver.ca/opendata/xml/weekendplayfieldstatus.xml
 
-private URL theURL;
+	public WeekendPlayfieldStatusParser() {
 
-public WeekendPlayfieldStatusParser(URL aURL) {
-	theURL = aURL;
-}
+	}
 
-//TODO: Implement this class
+	public void parse() {
+		// TODO: Implement this method using helper methods below
+
+	}
+
+	/**
+	 * Called to return the Weekend Playfield Status data from the CoV server
+	 * from URL:
+	 * ftp://webftp.vancouver.ca/opendata/xml/weekendplayfieldstatus.xml
+	 * 
+	 * @return a String representing the XML file from the City of Vancouver
+	 *         Data Catalogue
+	 */
+
+	private String downloadXMLString() {
+		String file = new String();
+		StringBuffer tempFile = new StringBuffer();
+
+		tempFile.setLength(0); // initialise the StringBuffer
+
+		try {
+			URL url = new URL(
+					"ftp://webftp.vancouver.ca/opendata/xml/weekendplayfieldstatus.xml");
+			BufferedReader reader = new BufferedReader(new InputStreamReader(
+					url.openStream()));
+			String individualLine;
+
+			while ((individualLine = reader.readLine()) != null) { // while
+																	// non-empty
+																	// lines
+																	// exist:
+				tempFile.append(individualLine); // read each line and add it to
+													// tempFile
+			}
+
+			reader.close();
+
+		} catch (MalformedURLException e) {
+			// Do nothing
+		} catch (IOException e) {
+			// Do nothing
+		}
+
+		file = tempFile.toString();
+		return file;
+	}
+
+	/**
+	 * Called to parse an XML String and create individual Park instances with
+	 * the data
+	 * 
+	 * @param file
+	 *            The XML file to be parsed
+	 */
+	private void parseXML(String file) {
+		// TODO: Complete this method
+	}
 
 }
