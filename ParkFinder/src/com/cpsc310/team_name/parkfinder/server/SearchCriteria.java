eafa9ea64da8adb6722d7a364b36a3fa97240bcc
Bo Ma <remus-ma@hotmail.com>
2013-03-28 17:03:44 -0700

    commit the changes

---
+++ ParkFinder/src/com/cpsc310/team_name/parkfinder/server/SearchCriteria.java
@@ -0,0 +1,38 @@
+package com.cpsc310.team_name.parkfinder.server;
+
+import java.util.ArrayList;
+
+public class SearchCriteria {
+	private ArrayList<String> stringList;
+	
+	public SearchCriteria() {
+	}
+	public void setSearchFacility(ArrayList<String> list)
+	{
+		stringList = list;
+	}
+	
+	public ArrayList<String> getList()
+	{
+		return stringList;
+	}
+		
+	
+	public String criteriaToSearch(String input){
+		String theType = null;
+		for(String str: stringList)
+		{
+			if(str.toLowerCase().contains(input.toLowerCase()));
+			{
+				theType = str;
+			}
+		}
+		return theType;
+		
+		
+	}
+
+	
+}
+
+
